b3cba0b3867b30928e1b0a20b009df93e20326f3
FedeBramb <146201363+FedeBramb@users.noreply.github.com>
2024-04-11 17:54:33 +0200

    update

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ class App extends Component {
 
   onButtonSubmit = () => { 
     this.setState({imageUrl: this.state.input})
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://smartbrainapi-etno.onrender.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -81,7 +81,7 @@ class App extends Component {
     .then(response => response.json())
     .then(result => {
       if (result) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://smartbrainapi-etno.onrender.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
