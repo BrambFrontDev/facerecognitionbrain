af2061f0a95d6f14b590878aec15645abdbe0904
BrambFrontDev <front.end.bramb@gmail.com>
2024-09-25 15:38:01 +0200

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('https://smartbrainapi-etno.onrender.com/register', {
+        fetch('https://smartbrainapi-tdxh.onrender.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
